e4fd3af1921596dc8554fc0b1b978e281e5f2685
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-30 11:48:39 +0100

    remove create and edit methods

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -19,16 +19,7 @@ class ItemsController extends Controller
         return response()->json($items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+    
     /**
      * Store a newly created resource in storage.
      *
@@ -67,17 +58,7 @@ class ItemsController extends Controller
         return response()->json($item);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
+    
     /**
      * Update the specified resource in storage.
      *
